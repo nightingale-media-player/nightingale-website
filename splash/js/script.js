a112449d88604cd0b90bf660399e711410043098
Martin Giger <martin.a.giger@sunrise.ch>
2011-12-10 00:06:08 +0100

    Some lots of changes. OS download buttons, animations etc.

--- splash/js/script.js
+++ splash/js/script.js
@@ -2,6 +2,9 @@
    Some GNU GPL License :D
 */
 
+var visible;
+var breaker = false;
+
 $(document).ready(function() {
 	$("#buttons img").mouseover(function() {
 		$(this).attr('src','img/'+$(this).attr('alt')+'_bg.png');
@@ -10,7 +13,65 @@ $(document).ready(function() {
 	$("#buttons img").mouseout(function() {
 		$(this).attr('src','img/'+$(this).attr('alt')+'.png');
 	});
+	
+	$("#screen").mouseover(function() {
+		if(!breaker) {
+			breaker= true;
+			$(this).animate({'height':$("#screen img").outerHeight()+'px'},200,function() {breaker=false;});
+			$("#spec").hide();
+		}
+	});
+	
+	$("#screen").mouseout(function() {
+		if(!breaker) {
+			breaker= true;
+			$("#spec").show();
+			$(this).animate({'height':'150px'},200,function() {breaker=false;});
+		}
+	});
+	
+	hideOS();
 });
+
+function getOS() {
+	var OSName="Unknown OS";
+	if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+	if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+	if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+	
+	return OSName;
+}
+
+function allOS() {
+	if(!visible) {
+		$("#linux").fadeIn();
+		$("#mac").fadeIn();
+		$("#windows").fadeIn();
+		$("#others").text("hide other OS");
+		visible=true;
+	}
+	else
+		hideOS();
+}
+
+function hideOS() {
+	switch(getOS()) {
+	case "Windows":
+		$("#linux").fadeOut();
+		$("#mac").fadeOut();
+		break;
+	case "Linux":
+		$("#windows").fadeOut();
+		$("#mac").fadeOut();
+		break;
+	case "MacOS":
+		$("#linux").fadeOut();
+		$("#windows").fadeOut();
+		break;
+	}
+	$("#others").text("other OS");
+	visible=false;
+}
 
 
 
