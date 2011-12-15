4508db4a894802d6802cccfa60ea3ed29b527640
Martin Giger <martin.a.giger@sunrise.ch>
2011-12-15 22:28:49 +0100

    * fix OS detection

--- static.getnightingale.com/js/script.js
+++ static.getnightingale.com/js/script.js
@@ -95,9 +95,9 @@ function getArchitecture() {
 
 function optimizeOS(OS,arch) {
 	var a=0;
-	do {
+	while (download.systems[a].name != OS && a < download.systems.length && download.systems[a].architecture == arch); {
 		a++;
-	} while (download.systems[a].name != OS && a < download.systems.length && download.systems[a].architecture == arch);
+	}
 	$('.button').attr('href',download.systems[a].link);
 	$('.button .small').text(OS+" | "+arch);
 	$('.button').click(function() {
