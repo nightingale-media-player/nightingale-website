0dac5ca3de1a36122c69eaeb0d9bb985c4344384
unknown <Martin@Martins-PC.(none)>
2012-03-01 19:32:54 +0100

    * Changed "play" button active state still sucks * Updated firstrun mockup

---
+++ getnightingale.com/firstrun/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html >
+<html>
+<head>
+	<title>Welcome to Nightingale!</title>
+	
+	<link rel="stylesheet" href="http://static.getnightingale.com/css/general.css" >
+	<link rel="stylesheet" href="http://static.getnightingale.com/css/style.css" >
+	<style type="text/css">
+	 #headtitle a {
+		color: lightgray;
+	 }
+	 
+	 .flist {
+		list-style: none;
+		padding:0;
+		margin:0 0 10px;
+	 }
+	 
+	 .flist li {
+		padding: 5px;
+		border-bottom: 1px solid grey;
+	 }
+	 
+	 #left {
+		width:455px;
+		padding: 10px;
+		height:342px;
+		border-right: 1px solid grey;
+	 }
+	 
+	 #right {
+		width:455px;
+		padding: 10px;
+		margin-left:475px;
+		margin-top:-362px;
+	 }
+	 
+	 #right h3 {
+		margin-bottom: 5px;
+		padding: 0;
+	 }
+	 
+	 #suglist {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	 }
+	 #suglist li {
+		padding: 10px 0;
+		border-bottom: 1px solid grey;
+	 }
+	 
+	 #suglist li h4 {
+		margin: 0;
+		padding: 0;
+	 }
+	 
+	 .addonicon {
+		height: 32px;
+		width: 32py;
+		margin-right: 3px;
+	 }
+	</style>
+	
+	<link type="image/vnd.microsoft.icon" size="32x32" rel="icon" href="http://getnightingale.com/favicon.ico" >
+  
+	<script src="http://static.getnightingale.com/js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+<body>
+	<div id="page">
+		<!-- Use this as header -->
+		<header id="ngalemainhead">
+			<a href="/" title="Home"><img src="http://static.getnightingale.com/img/Nightingale_text.png" id="headlogo" alt="Nightingale. the tune of Life, the tune of yours."></a>
+			<nav>
+			<ul class="menu">
+				<li><a href="http://getnightingale.com" title="Home">Home</a></li>
+				<li><a href="http://forum.getnightingale.com" title="Nightingale Forum">Forum</a></li>
+				<li><a href="http://addons.getnightingale.com" title="Addons for Nightingale">Addons</a></li>
+				<li><a href="http://blog.getnightingale.com" title="Development Blog">Blog</a></li>
+				<li><a href="http://wiki.getnightingale.com" title="Documentation and Wiki">Wiki</a></li>
+			</ul>
+			</nav>
+		</header>
+		<!-- end of header -->
+		<div id="main" role="main">
+			<div id="headtitle">
+				<h1>Welcome to Nightingale</h1>
+				More welcoming text. And more. More! MOREEEEEEEEEEEEEEEEEEEEEE! (less). <a href="http://wiki.getnightingale.com/doku.php?id=releases_notes:1.11.0_release_notes" title="Release Notes">Whats new?</a>
+			</div>
+			<div id="left">
+				<ul class="flist">
+					<li><img src="http://blogs-images.forbes.com/parmyolson/files/2011/06/ITunes_Logo.png" height="64" style="float:left;"> <b>Need help with importing your old library?</b><br>
+						We know that you want to get your music into Nightingale as fast as possible. For this purpose you can migrate your old itunes library to Nightingale.
+						<a href="http://wiki.getnightingale.com" class="more">Show me how &gt;</a></li>
+					<li>More helpingssss</li>
+				</ul>
+				<a href="http://wiki.getnightingale.com" class="more">Wiki &gt;</a>
+			</div>
+			<div id="right">
+				<p>
+					<img src="http://wiki.getnightingale.com/lib/exe/fetch.php?media=addon_generic.png" style="margin-right: 10px; float: left;">
+					Missing a feature? An addon may already do that...<br>
+					otherwise, you can help develop or suggest one!
+				</p>
+				<h3>Sugested Addons</h3>
+				<ul id="suglist">
+					<li><h4><img src="http://wiki.getnightingale.com/lib/exe/fetch.php?media=addon_mediaflow.png" class="addonicon" alt="Addon Icon">&nbsp;MediaFlow</h4>MediaFlow expands your experience of the library enormous by adding a flow of all your covers to the top of the library</li>
+					<li><h4><img src="http://code.google.com/p/songbird-livetweeter/logo?cct=1324873978" class="addonicon" alt="LiveTweeter Addon Icon">&nbsp;LiveTweeter</h4>Finally you can spam your skype, MSN, Twitter, Facebook and Google+ friends with the music you are listening to by just listening it. Because LiveTweeter spams for you.</li>
+				</ul>
+				<a href="http://addons.getnightingale.com" class="more">Show more Addons &gt;</a>
+			</div>
+		</div>
+	</div>
+	<?php include("http://static.getnightingale.com/footer.php"); ?>
+</body>
+</html>
