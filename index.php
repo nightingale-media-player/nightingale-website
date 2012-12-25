cfdbc45b4a4859ef95f7967da8e2150333304221
freaktechnik <martin.a.giger@sunrise.ch>
2012-12-25 18:26:59 +0100

    First steps with the grid
    This can change in the future.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTPYE html>
+<html>
+    <head>
+        <title>Nightingale - The tune of life, the tune of yours</title>
+    </head>
+    <body>
+        <nav>
+        </nav>
+        <div role="main" id="main">
+            <div id="wrapper" class="container center">
+            </div>
+        </div>
+        <footer class="container center">
+        </footer>
+    </body>
+</html>
