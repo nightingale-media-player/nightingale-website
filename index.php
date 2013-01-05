82d760604a27e2a2737752a30fb32e5093614770
freaktechnik <martin.a.giger@sunrise.ch>
2013-01-05 19:42:33 +0100

    Setup basic structure and grid

--- index.php
+++ index.php
@@ -2,15 +2,77 @@
 <html>
     <head>
         <title>Nightingale - The tune of life, the tune of yours</title>
+        <link rel="stylesheet" href="stylesheets/screen.css">
     </head>
     <body>
-        <nav>
-        </nav>
-        <div role="main" id="main">
             <div id="wrapper" class="container center">
+                <header>
+                    <nav>
+                    </nav>
+                </header>
+                <div id="main">
+                    <article>
+                        <div id="screenshots"></div>
+                        <div id="description">
+                            <span>Nightingale is a community support project for the powerful media player Songbird. It is developed by a proud community and we are equally proud to bting you the most extensible</span>
+                            <span>and feature-rich media experience. Freaturing smart plylists, equalizer, Last.fm integration, customizeable look and hundreds of add-ons. Nightingale has it all.</span>
+                        </div>
+                    </article>
+                    <aside>
+                        <div class="feature">
+                            <img src="http://lorempixel.com/400/400">
+                            <span>Lorem ipsum</span>
+                        </div>
+                        <div class="feature">
+                            <img src="http://lorempixel.com/404/404">
+                            <span>Lorem ipsum</span>
+                        </div>
+                        <div class="feature">
+                            <img src="http://lorempixel.com/401/401">
+                            <span>Lorem ipsum</span>
+                        </div>
+                    </aside>
+                </div>
+                <footer>
+                    <div id="footerinfo">
+                        <img>
+                        <p>
+                            Nightingale is free!<br>
+                            It is an Open Source projcet release under the terms of the GNU General Public License vX (GPL cX).<br>
+                            For more deails, please read the <a href="">license informations</a>.
+                        </p>
+                    </div>
+                    <div class="footerlinks">
+                        <b>Support</b>
+                        <ul>
+                            <li><a href="http://forum.getnightingale.com" title="Nightingale Forum">Community Forum</a></li>
+                            <li><a href="http://blog.getnightingale.com" title="Development Blog">Official Blog</a></li>
+                            <li><a href="http://addons.getnightingale.com" title="Addons for Nightingale">Addons</a></li>
+                            <li><a href="http://wiki.getnightingale.com" title="Nightingale Wiki">Wiki</a></li>
+                            <li><a href="http://forum.getnightingale.com/forum-13.html" title="Help" id="forumhelplink">Help Forum</a></li>
+                        </ul>
+                    </div>
+                    <div class="footerlinks">
+                        <b>Contribute</b>
+                        <ul>
+                            <li><a href="http://wiki.getnightingale.com" title="Documentation and Wiki">Developers Center</a></li>
+                            <li><a href="http://wiki.getnightingale.com/doku.php?id=ngale-locales" title="Translate Nightingale">Translate Nightingale</a></li>
+                            <li><a href="https://github.com/nightingale-media-player" title="GitHub">Source Code</a></li>
+                            <li><a href="http://bugs.getnightingale.com" title="Bugzilla">Report a Bug</a></li>
+                            <li><a href="http://getnightingale.com/donate" title="donate to Nightingale">Donate</a></li>
+                        </ul>
+                    </div>
+                    <div class="footerlinks">
+                        <b>MOAR!</b>
+                        <ul>
+                            <li><a href="http://wiki.getnightingale.com" title="Documentation and Wiki">Developers Center</a></li>
+                            <li><a href="http://wiki.getnightingale.com/doku.php?id=ngale-locales" title="Translate Nightingale">Translate Nightingale</a></li>
+                            <li><a href="https://github.com/nightingale-media-player" title="GitHub">Source Code</a></li>
+                            <li><a href="http://bugs.getnightingale.com" title="Bugzilla">Report a Bug</a></li>
+                            <li><a href="http://getnightingale.com/donate" title="donate to Nightingale">Donate</a></li>
+                        </ul>
+                    </div>
+                </footer>
             </div>
-        </div>
-        <footer class="container center">
-        </footer>
     </body>
 </html>
