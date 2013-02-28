d28da3492d11174fc95b9f5fff17ff187eb87a46
freaktechnik <martin.a.giger@sunrise.ch>
2013-02-28 01:03:27 +0100

    Finish header
    And other fixes

--- index.php
+++ index.php
@@ -6,52 +6,58 @@
     </head>
     <body>
         <div class="wrapper">
-            <header class="center container">
-                <nav role="navigation">
-                    <menu label="Navigation">
-                        <!-- use buttons? -->
-                        <li class="current"><a href="#">Home</a></li>
-                        <li><a href="#">Forum</a></li>
-                    </menu>
-                </nav>
-                <figure id="headerlogo">
-                    <img src="images/ngaleslogan.png">
-                </figure>
-            </header>
+            <div class="center">
+                <header class="container">
+                    <nav role="navigation">
+                        <menu label="Navigation">
+                            <!-- use buttons? -->
+                            <li class="current"><a href="#">Home</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Add-ons</a></li>
+                            <li><a href="#">Forum</a></li>
+                            <li><a href="#">Wiki</a></li>
+                            <li><a href="#">Developers</a></li>
+                        </menu>
+                    </nav>
+                    <figure id="headerlogo">
+                        <div id="shadowtab"></div>
+                        <div id="birdtab"></div>
+                        <img src="images/nightingale_official_text_outline.svg">
+                    </figure>
+                </header>
+            </div>
         </div>
         <div class="wrapper" id="wrapper">
             <section id="main" class="center container" role="main">
                 <article>
                     <div id="screenshots"></div>
                     <div id="description">
-                        <div>Nightingale is a community support project for the powerful media player Songbird. It is developed by a proud community and we are equally proud to bting you the most extensible</div>
-                        <div>and feature-rich media experience. Freaturing smart plylists, equalizer, Last.fm integration, customizeable look and hundreds of add-ons. Nightingale has it all.</div>
+                    Nightingale is a community support project for the powerful media player Songbird. It is developed by a proud community and we are equally proud to bting you the most extensible and feature-rich media experience. Freaturing smart playlists, equalizer, Last.fm integration, customizeable look and hundreds of add-ons. Nightingale has it all.
                     </div>
                 </article>
                 <aside>
-                    <div class="feature">
+                    <figure class="feature">
                         <img src="http://lorempixel.com/400/400">
-                        <span>Lorem ipsum</span>
-                    </div>
-                    <div class="feature">
+                        <figcaption>Lorem ipsum</figcaption>
+                    </figure>
+                    <figure class="feature">
                         <img src="http://lorempixel.com/404/404">
-                        <span>Lorem ipsum</span>
-                    </div>
-                    <div class="feature">
+                        <figcaption>Lorem ipsum</figcaption>
+                    </figure>
+                    <figure class="feature">
                         <img src="http://lorempixel.com/401/401">
-                        <span>Lorem ipsum</span>
-                    </div>
+                        <figcaption>Lorem ipsum</figcaption>
+                    </figure>
                 </aside>
             </section>
         </div>
         <div class="wrapper">
             <footer class="center container">
                 <section id="footerinfo">
-                    <img>
-                    <p>
-                        Nightingale is free!<br>
-                        It is an Open Source projcet release under the terms of the GNU General Public License vX (GPL cX).<br>
-                        For more deails, please read the <a href="">license informations</a>.
+                    <img id="footergale" src="images/nightingale_official_stroke_white.png">
+                    <p>Nightingale is free!<br>
+                       It is an Open Source projcet release under the terms of the GNU General Public License vX (GPL cX).<br>
+                       For more deails, please read the <a href="">license information</a>.
                     </p>
                 </section>
                 <nav class="footerlinks">
