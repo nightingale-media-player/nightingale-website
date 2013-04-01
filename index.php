d09f41ce472806d2948b49499734005cd0df2215
freaktechnik <martin.a.giger@sunrise.ch>
2013-04-01 20:46:47 +0200

    Making the page localizeable

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
-<?php
-    $name = 'Nightingale';
-    $title = 'The tune of life, the tune of yours';
-    $description = 'Nightingale is a community support project for the powerful media player Songbird. It is developed by a proud community and we are equally proud to bring you the most extensible and feature-rich media experience. Freaturing smart playlists, equalizer, Last.fm integration, customizeable look and hundreds of add-ons. Nightingale has it all.';
-    
-    
+<?php    
     $download['windows']['url'] = ''; // download url
     $download['windows']['img'] = 'images/wine.png'; // symbolicon
     $download['windows']['osname'] = 'Windows';
@@ -88,11 +83,11 @@
 ?>
 <!DOCTYPE html>
 <html>
-    <head lang="en">
+    <head>
         <!-- meta info -->
         <meta charset="utf-8">
-        <title><?php echo $name.' - '.$title;?></title>
-        <meta name="description" content="<?php echo $description; ?>">
+        <title>Nightingale - The tune of life, the tune of yours</title>
+        <meta name="description" content="Nightingale is a community support project for the powerful media player Songbird. It is developed by a proud community and we are equally proud to bring you the most extensible and feature-rich media experience. Freaturing smart playlists, equalizer, Last.fm integration, customizeable look and hundreds of add-ons. Nightingale has it all.">
         <meta http-equiv="X-UA-Compatible" content="chrome=1"> 
         
         <!-- styles -->
@@ -104,15 +99,20 @@
             <link rel="stylesheet" href="css/legacy-ie.css">
             <script src="javascript/html5shiv.js"></script>
         <![endif]-->
+        
+        <!-- l10n -->
+        <script src="javascript/l10n.js"></script>
+        <link rel="resource" type="application/l10n" href="l10n/locales.ini" >
+        
     </head>
     <body>
         <div  id="instructions">
             <section>
                 <ol type="1">
-                    <li>Open a terminal window</li>
-                    <li>Type <code>sudo add-apt-repository ppa:nightingaleteam/nightingale-release</code></li>
-                    <li>Then <code>sudo apt-get update</code></li>
-                    <li>And finally <code>sudo apt-get install nightingale</code></li>
+                    <li data-l10n-id="ubuntuFirstStep">Open a terminal window</li>
+                    <li><span data-l10n-id="ubuntuSecondStep">Type</span <code>sudo add-apt-repository ppa:nightingaleteam/nightingale-release</code></li>
+                    <li><span data-l10n-id="ubuntuThirdStep">Then</span> <code>sudo apt-get update</code></li>
+                    <li><span data-l10n-id="ubuntuFourthStep">And finally</span> <code>sudo apt-get install nightingale</code></li>
                 </ol>
             </section>
         </div>
@@ -121,20 +121,20 @@
         <div id="ngalemainheadwrapper" class="wrapper">
             <header class="container">
                 <nav role="navigation">
-                    <button class="mobilenav" id="expandngalenav">Navigation</button>
+                    <button class="mobilenav" id="expandngalenav" data-l10n-id="menu">Menu</button>
                     <ul id="ngalenavlist">
-                        <li class="current"><a href="#">Home</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Add-ons</a></li>
-                        <li><a href="#">Forum</a></li>
-                        <li><a href="#">Wiki</a></li>
-                        <li><a href="#">Developers</a></li>
+                        <li class="current"><a href="http://getnightingale.com" data-l10n-id="home">Home</a></li>
+                        <li><a href="http://blog.getnightingale.com" data-l10n-id="blog">Blog</a></li>
+                        <li><a href="http://addons.getnightingale.com" data-l10n-id="add-ons">Add-ons</a></li>
+                        <li><a href="http://forum.getnightingale.com" data-l10n-id="forum">Forum</a></li>
+                        <li><a href="http://wiki.getnightingale.com" data-l10n-id="wiki">Wiki</a></li>
+                        <li><a href="http://developers.getnightingale.com" data-l10n-id="developers">Developers</a></li>
                     </ul>
                 </nav>
                 <figure id="headerlogo" role="banner">
                     <div id="tabshadow" class="tab"></div>
                     <div id="birdtab" class="tab"></div>
-                    <img src="images/nightingale_official_text_outline.png" alt="Nightingale - The tune of life, the tune of yours">
+                    <img src="images/nightingale_official_text_outline.png" alt="Nightingale - The tune of life, the tune of yours" data-l10n-id="headerlogo">
                 </figure>
             </header>
         </div>
@@ -146,7 +146,7 @@
                         <img id="screenshottwo" src="http://lorempixel.com/501/301">
                         <button id="downloadbutton" <?php if($download[$osstring]['popup']) echo 'data-popup'; else echo 'data-url="'.$download[$osstring]['url'].'"'; ?>>
                             <img src="<?php echo $download[$osstring]['img']; ?>" alt="<?php echo $download[$osstring]['osname']; ?> Icon">
-                            <div><b>Download Nightingale</b><br>
+                            <div><b data-l10n-id="download">Download Nightingale</b><br>
                                 <small><?php 
                                     if($osstring!='unknown') {
                                         echo $download[$osstring]['arch'].'-bit | '.$download[$osstring]['osname'].' '.$download[$osstring]['package'];
@@ -157,25 +157,25 @@
                                 ?></small>
                             </div>
                         </button>
-                        <a href="#" id="moredownloadslink">Other platforms and architectures</a>
+                        <?php if($osstring!='unknown') echo '<a href="/download.php" id="moredownloadslink" data-l10n-id="otherPlattforms">Other platforms and architectures</a>'; ?>
                     </div>
-                    <div id="description">
-                        <?php echo $description; ?>
+                    <div id="description" data-l10n-id="description">
+                        Nightingale is a community support project for the powerful media player Songbird. It is developed by a proud community and we are equally proud to bring you the most extensible and feature-rich media experience. Freaturing smart playlists, equalizer, Last.fm integration, customizeable look and hundreds of add-ons. Nightingale has it all.
                     </div>
                 </div>
                 <aside>
-                    <h2>Why Nightingale?</h2>
+                    <h2 data-l10n-id="asideHeading">Why Nightingale?</h2>
                     <figure class="feature">
-                        <img src="http://lorempixel.com/400/400">
-                        <figcaption>Lorem ipsum</figcaption>
+                        <img src="http://lorempixel.com/400/400" alt="" data-l10n-id="firstFeatureImage">
+                        <figcaption data-l10n-id="firstFeature">Nightingale supports a wide variety of music and video formats, including MP3, Ogg, Flac, WAV and many more.</figcaption>
                     </figure>
                     <figure class="feature">
-                        <img src="http://lorempixel.com/404/404">
-                        <figcaption>Lorem ipsum</figcaption>
+                        <img src="http://lorempixel.com/404/404" alt="" data-l10n-id="secondFeatureImage">
+                        <figcaption data-l10n-id="secondFeature">Highly customizable user interface due to hundreds of themes and add-ons</figcaption>
                     </figure>
                     <figure class="feature">
-                        <img src="http://lorempixel.com/401/401">
-                        <figcaption>Lorem ipsum</figcaption>
+                        <img src="http://lorempixel.com/401/401" alt="" data-l10n-id="thirdFeatureImage">
+                        <figcaption data-l10n-id="thirdFeature">Support by an active and very ambitious community.</figcaption>
                     </figure>
                 </aside>
             </article>
@@ -183,42 +183,47 @@
         <div class="wrapper" id="ngalemainfooterwrapper">
             <footer class="container">
                 <section id="footerinfo" role="contentinfo">
-                    <img id="footergale" alt="white outlined nightingale project logo" src="images/footergale.png">
-                    <p>Nightingale is free!<br>
+                    <div id="leftfooter">
+                        <img id="footergale" alt="white outlined nightingale project logo" src="images/footergale.png">
+                        <select id="l10nselect">
+                            <option selected value="en">English</option>
+                        </select>
+                    </div>
+                    <p data-l10n-id="footerInfo">Nightingale is free!<br>
                        It is an Open Source projcet release under the terms of the GNU General Public License vX (GPL cX).<br>
                        For more deails, please read the <a href="">license information</a>.
                    </p>
                    <p>
-                       <b>Follow us!</b><br>
+                       <b data-l10n-id="footerSocial">Follow us!</b><br>
                        <a href="http://www.facebook.com/pages/Nightingale/210174055669535" title="Nightingale on Facebook"><img class="socialicon" src="images/facebook.png" alt="Like Nightingale on Facebook"></a><a href="https://plus.google.com/103377471329459083108/posts" title="Nightingale on Google+"><img class="socialicon" src="images/googlep.png" alt="Circle Nightingale on Google+"></a><a href="http://twitter.com/getnightingale" title="Nightingale on Twitter"><img class="socialicon" src="images/twitter.png" alt="Follow @getnightingale on Twitter"></a>
                    </p> 
                 </section>
                 <nav class="footerlinks">
-                    <b>Support</b>
+                    <b data-l10n-id="footerSupport">Support</b>
                     <ul>
-                        <li><a href="http://forum.getnightingale.com" title="Nightingale Forum">Community Forum</a></li>
-                        <li><a href="http://blog.getnightingale.com" title="Development Blog">Official Blog</a></li>
-                        <li><a href="http://addons.getnightingale.com" title="Addons for Nightingale">Addons</a></li>
-                        <li><a href="http://wiki.getnightingale.com" title="Nightingale Wiki">Wiki</a></li>
-                        <li><a href="http://forum.getnightingale.com/forum-13.html" title="Help" id="forumhelplink">Help Forum</a></li>
+                        <li><a href="http://forum.getnightingale.com" title="Nightingale Forum" data-l10n-id="footerForum">Community Forum</a></li>
+                        <li><a href="http://blog.getnightingale.com" title="Development Blog" data-l10n-id="footerBlog">Official Blog</a></li>
+                        <li><a href="http://addons.getnightingale.com" title="Add-ons for Nightingale" data-l10n-id="footerAdd-ons">Add-ons</a></li>
+                        <li><a href="http://wiki.getnightingale.com" title="Nightingale Wiki" data-l10n-id="footerWiki">Wiki</a></li>
+                        <li><a href="http://forum.getnightingale.com/forum-13.html" title="Help" id="forumhelplink" data-l10n-id="footerHelpForum">Help Forum</a></li>
                     </ul>
                 </nav>
                 <nav class="footerlinks">
-                    <b>Contribute</b>
+                    <b data-l10n-id="footerContribute">Contribute</b>
                     <ul>
-                        <li><a href="http://wiki.getnightingale.com" title="Documentation and Wiki">Developers Center</a></li>
-                        <li><a href="http://wiki.getnightingale.com/doku.php?id=ngale-locales" title="Translate Nightingale">Translate Nightingale</a></li>
-                        <li><a href="https://github.com/nightingale-media-player" title="GitHub">Source Code</a></li>
-                        <li><a href="http://bugs.getnightingale.com" title="Bugzilla">Report a Bug</a></li>
-                        <li><a href="http://getnightingale.com/donate" title="donate to Nightingale">Donate</a></li>
+                        <li><a href="http://wiki.getnightingale.com" title="Documentation and Wiki" data-l10n-id="footerDeveloperCenter">Developer's Center</a></li>
+                        <li><a href="http://wiki.getnightingale.com/doku.php?id=ngale-locales" title="Translate Nightingale" data-l10n-id="footerTranslate">Translate Nightingale</a></li>
+                        <li><a href="https://github.com/nightingale-media-player" title="Source Code on GitHub" data-l10n-id="footerSource">Source Code</a></li>
+                        <li><a href="https://github.com/nightingale-media-player/nightingale-addons/issues/" title="Nightingale Issues on GitHub" data-l10n-id="footerBugs">Report a Bug</a></li>
+                        <!--<li><a href="http://getnightingale.com/donate" title="Donate to Nightingale" data-l10n-id="footerDonate">Donate</a></li>-->
                     </ul>
                 </nav>
                 <nav class="footerlinks">
-                    <b>Ressources</b>
+                    <b data-l10n-id="footerRessources">Ressources</b>
                     <ul>
-                        <li><a href="http://wiki.getnightingale.com" title="Documentation and Wiki">Download Nightingale</a></li>
-                        <li><a href="http://wiki.getnightingale.com/doku.php?id=ngale-locales" title="Translate Nightingale">Features</a></li>
-                        <li><a href="https://github.com/nightingale-media-player" title="GitHub">Nightlies</a></li>
+                        <li><a href="http://getnightingale.com/download.php" title="Download Nightingale" data-l10n-id="footerDownload">Download Nightingale</a></li>
+                        <li><a href="http://getnightingale.com/features.php" title="Nightingale Features" data-l10n-id="footerFeatures">Features</a></li>
+                        <li><a href="http://getnightingale.com/nightlies.php" title="Nightingale Nightlies" data-l10n-id="footerNightlies">Nightlies</a></li>
                     </ul>
                 </nav>
             </footer>
@@ -241,6 +246,28 @@
                     });
                 }
                 document.getElementById("expandngalenav").addEventListener("click",toggleNav);
+                
+                var l10n = document.webL10n;
+                
+                l10n.ready(function() {
+                
+                    // add the languages to the dropdown
+                    var langs = l10n.getLanguages();
+                    var select = document.getElementById('l10nselect'), n;
+                    for(var l in langs) {
+                        n = document.createElement("option");
+                        n.innerHTML = langs[l];
+                        select.append(n);
+                    }
+                    
+                    // set current language
+                    select.value = l10n.getLanguage(); // not working with IE<9
+                    
+                    // chane document language when selection is changed
+                    select.onchange = function() {
+                        l10n.setLanguage(this.value);
+                    };
+                });
             };
             
             function hideOverlay() {
