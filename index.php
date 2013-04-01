e6dd9436952d2ee19a6b1e538cadc11a9fffb6ea
freaktechnik <martin.a.giger@sunrise.ch>
2013-04-01 16:59:20 +0200

    Add icons

--- index.php
+++ index.php
@@ -5,42 +5,49 @@
     
     
     $download['windows']['url'] = ''; // download url
-    $download['windows']['img'] = ''; // symbolicon
+    $download['windows']['img'] = 'images/wine.png'; // symbolicon
     $download['windows']['osname'] = 'Windows';
     $download['windows']['arch'] = 32;
     $download['windows']['package'] = '.exe'; // orange package info
     $download['windows']['popup'] = false;
     
     $download['mac']['url'] = '';
-    $download['mac']['img'] = '';
+    $download['mac']['img'] = 'images/dmg.png';
     $download['mac']['osname'] = 'Mac OS X';
     $download['mac']['arch'] = 32;
     $download['mac']['package'] = '.dmg';
     $download['mac']['popup'] = false;
     
     $download['ubuntu']['url'] = '';
-    $download['ubuntu']['img'] = '';
+    $download['ubuntu']['img'] = 'images/start-here-ubuntuoriginal.png';
     $download['ubuntu']['osname'] = 'Ubuntu';
     $download['ubuntu']['arch'] = getArch();
     $download['ubuntu']['package'] = 'PPA';
     $download['ubuntu']['popup'] = true;
     
-    $download['arch']['url'] = '';
-    $download['arch']['img'] = '';
+    $download['debian']['url'] = '';
+    $download['debian']['img'] = 'imges/application-x-deb.png';
+    $download['debian']['osname'] = 'Debian';
+    $download['debian']['arch'] = 32;
+    $download['debian']['package'] = '.deb';
+    $download['debian']['popup'] = false;
+    
+    $download['arch']['url'] = 'https://aur.archlinux.org/packages/ni/nightingale-git/nightingale-git.tar.gz';
+    $download['arch']['img'] = 'images/application-x-generic.png';
     $download['arch']['osname'] = 'Archlinux';
     $download['arch']['arch'] = 32;
     $download['arch']['package'] = 'PKGBUILD';
     $download['arch']['popup'] = false;
     
     $download['linux32']['url'] = '';
-    $download['linux32']['img'] = '';
+    $download['linux32']['img'] = 'images/application-x-generic.png';
     $download['linux32']['osname'] = 'Linux';
     $download['linux32']['arch'] = 32;
     $download['linux32']['package'] = '.tar.bz2';
     $download['linux32']['popup'] = false;
     
     $download['linux64']['url'] = '';
-    $download['linux64']['img'] = '';
+    $download['linux64']['img'] = 'images/application-x-generic.png';
     $download['linux64']['osname'] = 'Linux';
     $download['linux64']['arch'] = 64;
     $download['linux64']['package'] = '.tar.bz2';
@@ -54,6 +61,8 @@
             $os = 'mac';
         elseif(preg_match('/ubuntu/i', $_SERVER['HTTP_USER_AGENT']))
             $os = 'ubuntu';
+        elseif(preg_match('/debian/i', $_SERVER['HTTP_USER_AGENT']))
+            $os = 'debian';
         elseif(preg_match('/archlinux/i', $_SERVER['HTTP_USER_AGENT']))
             $os = 'arch';
         elseif(preg_match('/linux/i', $_SERVER['HTTP_USER_AGENT']))
