9803a44476d2605d17c8e213d1646d81134fbef9
freaktechnik <martin.a.giger@sunrise.ch>
2013-11-15 14:10:42 +0100

    Oops

---
+++ dashboard.getnightingale.com/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- meta info -->
+        <meta charset="utf-8">
+        <title data-l10n-id="dashboardTitle">Nightingale Dashboard</title>
+        <meta name="description" content="Nightingale is a community support project for the powerful media player Songbird. It is developed by a proud community and we are equally proud to bring you the most extensible and feature-rich media experience. Freaturing smart playlists, equalizer, Last.fm integration, customizeable look and hundreds of add-ons. Nightingale has it all.">
+        <meta http-equiv="X-UA-Compatible" content="chrome=1"> 
+        
+        <!-- styles -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" type="text/css" href="../static.getnightingale.com/css/style.css">
+        <!--[if lt IE 9]>
+            <link rel="stylesheet" href="css/legacy-ie.css">
+            <script src="javascript/html5shiv.js"></script>
+        <![endif]-->
+        
+        <!-- l10n -->
+        <script type="text/javascript" src="../static.getnightingale.com/javascript/l10n.js"></script>
+        <script type="text/javascript" src="../static.getnightingale.com/javascript/base.js"></script>
+        <link rel="prefetch" type="application/l10n" href="../static.getnightingale.com/l10n/locales.ini" >
+        
+        <!-- status dashboard scripts -->
+        <script type="text/javascript" src="../static.getnightingale.com/javascript/crowd-dashboard.js"></script>
+        <script type="text/javascript">
+            "use strict";
+
+            var statusDashboard;
+            window.onload = function() {
+                function loadDataForDashboard(fileURL,dashboard) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.onreadystatechange = function() {
+                        if( xhr.readyState == 4 && xhr.status != 0 && xhr.status < 400 )
+                            dashboard.setServers(JSON.parse(xhr.response));
+                        else
+                            dashboard.setServers([]);
+                    };
+
+                    xhr.open('GET',fileURL);
+                    xhr.send();
+                }
+
+                statusDashboard = new Dashboard();
+                loadDataForDashboard("../static.getnightingale.com/javascript/servers.json",statusDashboard);
+            };
+
+            function refresh(dashboard) {
+                dashboard.checkServers();
+            }
+        </script>        
+    </head>
+    <body>
+        <div id="ngalemainheadwrapper" class="wrapper">
+            <header class="container">
+                <nav role="navigation">
+                    <button class="mobilenav" id="expandngalenav" data-l10n-id="menu">Menu</button>
+                    <ul id="ngalenavlist">
+                        <li><a href="http://getnightingale.com" data-l10n-id="home">Home</a></li>
+                        <li><a href="http://blog.getnightingale.com" data-l10n-id="blog">Blog</a></li>
+                        <li><a href="http://addons.getnightingale.com" data-l10n-id="add-ons">Add-ons</a></li>
+                        <li><a href="http://forum.getnightingale.com" data-l10n-id="forum">Forum</a></li>
+                        <li><a href="http://wiki.getnightingale.com" data-l10n-id="wiki">Wiki</a></li>
+                        <li><a href="http://developers.getnightingale.com" data-l10n-id="developers">Developers</a></li>
+                    </ul>
+                </nav>
+                <figure id="headerlogo" role="banner">
+                    <div id="tabshadow" class="tab"></div>
+                    <div id="birdtab" class="tab"></div>
+                    <img src="../static.getnightingale.com/images/nightingale_official_text_outline.png" alt="Nightingale - The tune of life, the tune of yours" data-l10n-id="headerlogo" data-hdpi>
+                </figure>
+            </header>
+        </div>
+        <div class="wrapper" id="wrapper">
+            <article id="main" class="container" role="main">
+                <section class="twocolumns">
+                    <h1>Statistics</h1>
+                    <h2>Installations &amp; Upgrades</h2>
+                    <img src="http://humanoids.be/stats/?module=API&method=ImageGraph.get&idSite=3&apiModule=CustomVariables&apiAction=getCustomVariablesValuesFromNameId&idSubtable=Type&language=en&token_auth=506ec0949c33349acc434455dd7e9e75&period=month&date=today">
+                    <h2>Version</h2>
+                    <img src="http://humanoids.be/stats/?module=API&method=ImageGraph.get&idSite=3&apiModule=CustomVariables&apiAction=getCustomVariablesValuesFromNameId&idSubtable=Version&language=en&token_auth=506ec0949c33349acc434455dd7e9e75&period=month&date=today">
+                </section>
+                <section class="column">
+                    <h1>Services Status</h1>
+                    <button onclick="refresh(statusDashboard);">Refresh</button>
+                    <div id="crowd-dashboard-status-list">
+                    </div>
+                </section>
+            </article>
+        </div>
+        <div class="wrapper" id="ngalemainfooterwrapper">
+            <footer class="container">
+                <section id="footerinfo" role="contentinfo">
+                    <div id="leftfooter">
+                        <img id="footergale" alt="white outlined nightingale project logo" src="../static.getnightingale.com/images/footergale.png" data-hdpi>
+                        <select id="l10nselect">
+                            <option selected value="en">English</option>
+                        </select>
+                    </div>
+                    <p data-l10n-id="footerInfo" data-l10n-args='{"license":"GNU General Public License v2 (GPL v2)"}'>Nightingale is free!<br>
+                       It is an Open Source projcet release under the terms of the GNU General Public License v2 (GPL v2).<br>
+                       For more details, please read the <a href="">license information</a>.
+                   </p>
+                   <p>
+                       <b data-l10n-id="footerSocial">Follow us!</b><br>
+                       <a href="http://www.facebook.com/getnightingale" title="Nightingale on Facebook" class="socialicon" rel="me">f</a>&nbsp;
+                       <a href="https://plus.google.com/+Getnightingale" title="Nightingale on Google+" class="socialicon" rel="me">g</a>&nbsp;<a href="http://twitter.com/getnightingale" title="Nightingale on Twitter" class="socialicon" rel="me">t</a>
+                   </p> 
+                </section>
+                <nav class="footerlinks">
+                    <b data-l10n-id="footerSupport">Support</b>
+                    <ul>
+                        <li><a href="http://forum.getnightingale.com" title="Nightingale Forum" data-l10n-id="footerForum">Community Forum</a></li>
+                        <li><a href="http://blog.getnightingale.com" title="Development Blog" data-l10n-id="footerBlog">Official Blog</a></li>
+                        <li><a href="http://addons.getnightingale.com" title="Add-ons for Nightingale" data-l10n-id="footerAdd-ons">Add-ons</a></li>
+                        <li><a href="http://wiki.getnightingale.com" title="Nightingale Wiki" data-l10n-id="footerWiki">Wiki</a></li>
+                        <li><a href="http://forum.getnightingale.com/forum-13.html" title="Help" id="forumhelplink" data-l10n-id="footerHelpForum">Help Forum</a></li>
+                    </ul>
+                </nav>
+                <nav class="footerlinks">
+                    <b data-l10n-id="footerContribute">Contribute</b>
+                    <ul>
+                        <li><a href="http://wiki.getnightingale.com" title="Documentation and Wiki" data-l10n-id="footerDeveloperCenter">Developer's Center</a></li>
+                        <li><a href="http://wiki.getnightingale.com/doku.php?id=ngale-locales" title="Translate Nightingale" data-l10n-id="footerTranslate">Translate Nightingale</a></li>
+                        <li><a href="https://github.com/nightingale-media-player" title="Source Code on GitHub" data-l10n-id="footerSource">Source Code</a></li>
+                        <li><a href="https://github.com/nightingale-media-player/nightingale-addons/issues/" title="Nightingale Issues on GitHub" data-l10n-id="footerBugs">Report a Bug</a></li>
+                        <!--<li><a href="http://getnightingale.com/donate" title="Donate to Nightingale" data-l10n-id="footerDonate">Donate</a></li>-->
+                    </ul>
+                </nav>
+                <nav class="footerlinks">
+                    <b data-l10n-id="footerRessources">Ressources</b>
+                    <ul>
+                        <li><a href="http://getnightingale.com/download.php" title="Download Nightingale" data-l10n-id="footerDownload">Download Nightingale</a></li>
+                        <li><a href="http://getnightingale.com/features.php" title="Nightingale Features" data-l10n-id="footerFeatures">Features</a></li>
+                        <li><a href="http://getnightingale.com/nightlies.php" title="Nightingale Nightlies" data-l10n-id="footerNightlies">Nightlies</a></li>
+                    </ul>
+                </nav>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ static.getnightingale.com/javascript/crowd-dashboard.js
@@ -0,0 +1,144 @@
+/**
+ * Credits for the status ping image hack idea to (even tough this might not be ther original source): http://jsfiddle.net/Maslow/GSSCD/
+ */
+
+"use strict";
+
+Dashboard.prototype.servers = [];
+Dashboard.prototype.elementId = "crowd-dashboard-status-list";
+Dashboard.prototype.count = 0;
+Dashboard.prototype.ready = -1;
+Dashboard.prototype.onready = null;
+
+function Dashboard(servers, elementId) {
+    this.servers = new Array();
+
+    if( servers ) {
+        this.setServers( servers );
+        if( elementId ) {
+            this.setTarget( elementId );
+        }
+
+        if(!this.isReady()) {
+            this.checkServers();
+        }
+    }
+}
+
+Dashboard.prototype.setServers = function(servers) {
+    if( typeof servers == "object" && servers.length > 0 ) {
+        this.servers = servers;
+        for( var serverList in this.servers ) {
+            this.count += this.servers[serverList].pages.length;
+        }
+
+        this.checkServers();
+    }
+    else if( typeof this.onready == "function" && this.onready != null )
+        this.onready();
+
+};
+
+Dashboard.prototype.setTarget = function(elementId) {
+    if( typeof elementId == "string" ) {
+        this.elementId = elementId;
+    }
+};
+
+Dashboard.prototype.checkServers = function() {
+    // not too nice way to do it, but it does the job
+    document.getElementById(this.elementId).innerHTML = 'Loading...';
+    this.ready = 0;
+
+    var that = this;
+    function getStatus(url, callback) {
+        var img = new Image();
+        var done = false;
+
+        img.onload = function() {
+            callback( url, true, that );
+            done=true;
+        };
+        img.onerror = function(e) {
+            //x-origin/no image
+            callback( url, true, that );
+            done=true;
+        };
+
+        setTimeout(function() {
+            if(!done)
+                callback( url, false, that );
+        }, 5000);
+
+        var rand = (url.contains('?')?'&':'?')+'timestamp='+Date.now();
+        img.src = url+rand;
+    }
+
+    for( var serverList in this.servers ) {
+        for( var page in this.servers[serverList].pages) {
+            getStatus(this.servers[serverList].pages[page].url, this.addServerToList);
+        }
+    }
+};
+
+Dashboard.prototype.addServerToList = function( url, online, that ) {
+    var page;
+    for( var serverList in that.servers ) {
+        for( var pageIndex in that.servers[serverList].pages) {
+            page = that.servers[serverList].pages[pageIndex];
+            if(page.url == url) {
+                page.online = online;
+                that.ready++;
+                break;
+            }
+        }
+    }
+
+    if(that.isReady()) {
+        document.getElementById(that.elementId).innerHTML = '';
+        that.createLists();
+        if( typeof that.onready == "function" && that.onready != null )
+            that.onready();
+    }
+};
+
+Dashboard.prototype.isReady = function() {
+    return this.count == this.ready;
+};
+
+Dashboard.prototype.clear = function() {
+    this.servers = {};
+    this.count = 0;
+    this.ready = -1;
+    // not too nice way to do it, but it does the job
+    document.getElementById(this.elementId).innerHTML = '';
+};
+
+Dashboard.prototype.createLists = function() {
+    var root = document.getElementById(this.elementId);
+    var heading, list, item, link;
+    for(var serverList in this.servers) {
+        heading = document.createElement('h2');
+        heading.appendChild(document.createTextNode(this.servers[serverList].name));
+        list = document.createElement('ul');
+        for(var page in this.servers[serverList].pages) {
+            item = document.createElement('li');
+            link = document.createElement('a');
+            link.href = this.servers[serverList].pages[page].url;
+            link.appendChild(document.createTextNode(this.servers[serverList].pages[page].name));
+            item.appendChild(link);
+            if(this.servers[serverList].withLocations) {
+                item.appendChild(document.createTextNode(" in "));
+                link = document.createElement('a');
+                link.classList.add('dashboard-location');
+                link.href = 'http://maps.google.com/?q='+this.servers[serverList].pages[page].location;
+                link.appendChild(document.createTextNode(this.servers[serverList].pages[page].location));
+                item.appendChild(link);
+            }
+            item.classList.add(this.servers[serverList].pages[page].online?'online':'offline');
+            list.appendChild(item);
+        }
+        root.appendChild(heading);
+        root.appendChild(list);
+    }
+};
