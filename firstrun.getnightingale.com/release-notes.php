681e4b1962d14ec05c70e122591e271cfb02397a
freaktechnik <martin.a.giger@sunrise.ch>
2014-01-11 13:50:25 +0100

    Add update release-notes

---
+++ firstrun.getnightingale.com/release-notes.php
@@ -0,0 +1,27 @@
+<?php
+    if(file_exists($_GET['version'].'.json'))
+        $content = json_decode(file_get_contents($_GET['version'].'.json'));
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link type="text/css" rel="stylesheet" media="screen" href="chrome://songbird-web/content/html.css" />
+    </head>
+    <body class="darkest">
+        <h1>Nightingale <?php echo $_GET['version']; ?> Release Notes</h1>
+        <?php
+        if(isset($content)) {
+            $list = '<ul>';
+            foreach($content->changes as $change) {
+                $list .= '<li>'.$change->title.'</li>
+                ';
+            }
+            echo $list.'</ul>';
+        }
+        else {
+            echo "<p>Sorry, we found no release notes for this version</p>";
+        }
+        ?>
+    </body>
+</html>
