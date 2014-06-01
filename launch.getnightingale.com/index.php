8e2bbddfc257c900cbb7e4299eb85378163f9b52
freaktechnik <martin.a.giger@sunrise.ch>
2014-06-01 19:25:02 +0200

    L10n for launch

--- launch.getnightingale.com/index.php
+++ launch.getnightingale.com/index.php
@@ -17,7 +17,7 @@ if (!($_COOKIE["nightingale_installed"] == "yes")) {
 <?php
 if ($_COOKIE["nightingale_installed"] == "yes") {
   echo	"
-  <title>Launching Nightingale</title>
+  <title data-l10n-id='launch_inprogress'>Launching Nightingale...</title>
 	<script type='text/javascript'>
 		function nightingaleOpen(stay) {
 			var gotourl = ".$_GET['url'].";
@@ -42,10 +42,13 @@ if ($_COOKIE["nightingale_installed"] == "yes") {
 	</script>  ";
   }
   else {
-    echo "<title>Got Nightingale?</title>";
+    echo "<title data-l10n-id='launch_question'>Got Nightingale?</title>";
   }
 ?>
 	<link rel='stylesheet' type='text/css' href='css/launch.css' />
+    
+    <script type="text/javascript" src="//static.getnightingale.com/javascript/l10n.js"></script>
+    <link rel="prefetch" type="application/l10n" href="//static.getnightingale.com/l10n/locales.ini">
 </head>
 
 <?php
@@ -53,8 +56,8 @@ if ($_COOKIE["nightingale_installed"] == "yes") {
   echo	"
   <body id='launch' onload='nightingaleOpen(false)'>
 	<div id='wrap'>
-		<h1>One Moment Please</h1>
-		<p class='note'>
+		<h1 data-l10n-id='launch_wait'>One Moment Please</h1>
+		<p class='note' data-l10n-id='launch_inprogress'>
 			Launching Nightingale...
 		</p>
 		<p class='link'>
@@ -63,7 +66,7 @@ if ($_COOKIE["nightingale_installed"] == "yes") {
 		<img src='images/launch.png' />
 		<div id='options'>
 			<form action='http://getnightingale.com/' method='GET'>
-				<input type='submit' value='Download Nightingale' />
+				<input type='submit' value='Download Nightingale' data-l10n-id='launch_download'/>
 			</form>
 		</div>
 	</div>
@@ -73,8 +76,8 @@ if ($_COOKIE["nightingale_installed"] == "yes") {
     echo "
       <body id='detect'>
         <div id='wrap'>
-          <h1>Got Nightingale?</h1>
-          <p class='note'>
+          <h1 data-l10n-id='launch_question'>Got Nightingale?</h1>
+          <p class='note' data-l10n-id='launch_nogale'>
           You tried to open the following link in Nightingale but
           Nightingale has not yet been detected.
         </p>
@@ -86,10 +89,10 @@ if ($_COOKIE["nightingale_installed"] == "yes") {
           <form action='http://" . $_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"] . "' method='POST'>
             <input type='hidden' name='have' value='yes' />
             <input type='hidden' name='url' value='".$_GET['url']."' />
-            <input type='submit' value='I have Nightingale'".($_GET['url']==''?" disabled='true'":'')." />
+            <input type='submit' value='I have Nightingale'".($_GET['url']==''?" disabled='true'":'')." data-l10n-id='launch_yes'/>
           </form>
           <form action='http://getnightingale.com/' method='GET'>
-            <input type='submit' value='Download Nightingale' />
+            <input type='submit' value='Download Nightingale' data-l10n-id='launch_download'/>
           </form>
         </div>
       </div>
