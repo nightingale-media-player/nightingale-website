58a071b20465c81c71f35e9a729853c706955447
Martin Giger <martin.a.giger@sunrise.ch>
2015-07-15 15:14:29 +0200

    Add tx config and pull all the translations in

--- static.getnightingale.com/php/head.php
+++ static.getnightingale.com/php/head.php
@@ -1,6 +1,6 @@
 <?php include_once "../static.getnightingale.com/php/env.php"; ?>
         <meta name="description" content="Nightingale is a community support project for the powerful media player Songbird. It is developed by a proud community and we are equally proud to bring you the most extensible and feature-rich media experience. Freaturing smart playlists, equalizer, Last.fm integration, customizeable look and hundreds of add-ons. Nightingale has it all.">
-        <meta http-equiv="X-UA-Compatible" content="chrome=1"> 
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
         <link type="text/plain" rel="author" href="http://getnightingale.com/humans.txt">
 
         <link type="image/vnd.microsoft.icon" sizes="32x32" rel="icon" href="favicon.ico">
@@ -18,3 +18,4 @@
         <script type="text/javascript" src="<? echo $protocol; ?>static.getnightingale.com/javascript/l10n.js"></script>
         <script type="text/javascript" src="<? echo $protocol; ?>static.getnightingale.com/javascript/base.js"></script>
         <link rel="prefetch" type="application/l10n" href="<?php echo $protocol; ?>static.getnightingale.com/l10n/locales.ini" >
+
