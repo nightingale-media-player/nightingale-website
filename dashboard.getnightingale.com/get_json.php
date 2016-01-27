26b189f33868fac26782796976cde0fdbcfc38b1
Martin Giger <martin@humanoids.be>
2016-01-27 11:24:04 +0100

    No e for you

--- dashboard.getnightingale.com/get_json.php
+++ dashboard.getnightingale.com/get_json.php
@@ -56,7 +56,7 @@ function getData($url) {
     header("Content-Type: application/json");
     header("Access-Control-Allow-Origin: http://dashboard.getnightingale.com");
     
-    $return = new stdClass;e
+    $return = new stdClass;
 
     switch($_GET['type']) {
         case 'versionInfo':
